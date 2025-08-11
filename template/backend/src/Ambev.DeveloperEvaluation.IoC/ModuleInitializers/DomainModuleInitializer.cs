6636d06b5a729cbf489f722a874c58a5e88f301a
LUCAS SANTANA <lucassantana_rs@hotmail.com>
2025-08-10 23:09:31 -0300

    Injector DependencyResolver

---
+++ template/backend/src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/DomainModuleInitializer.cs
@@ -0,0 +1,16 @@
+﻿using Ambev.DeveloperEvaluation.Common.Security;
+using Ambev.DeveloperEvaluation.Domain.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ambev.DeveloperEvaluation.IoC.ModuleInitializers;
+
+public class DomainModuleInitializer : IModuleInitializer
+{
+    public void Initialize(WebApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<ISaleDomainService, SaleDomainService>();
+        builder.Services.AddSingleton<ISaleItemDomainService, SaleItemDomainService>();
+        builder.Services.AddSingleton<IEventPublisherService, ConsoleEventPublisherService>();
+    }
+}
