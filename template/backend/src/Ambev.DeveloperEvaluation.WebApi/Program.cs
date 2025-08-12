bb378dfe98a14d0e73eced752cd315e11b3c7c1a
LUCAS SANTANA <lucassantana_rs@hotmail.com>
2025-08-12 09:11:25 -0300

    Create configuration migratios program

--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Program.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Program.cs
@@ -7,78 +7,88 @@ using Ambev.DeveloperEvaluation.IoC;
 using Ambev.DeveloperEvaluation.ORM;
 using Ambev.DeveloperEvaluation.WebApi.Middleware;
 using MediatR;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
-
 namespace Ambev.DeveloperEvaluation.WebApi;
 
 public class Program
 {
     public static void Main(string[] args)
+{
+    try
     {
-        try
+        Log.Information("Starting web application");
+
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        builder.AddDefaultLogging();
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.AddBasicHealthChecks();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddDbContext<DefaultContext>(options =>
+            options.UseNpgsql(
+                builder.Configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.ORM")
+            )
+        );
+
+        builder.Services.AddJwtAuthentication(builder.Configuration);
+
+        builder.RegisterDependencies();
+
+        builder.Services.AddAutoMapper(typeof(Program).Assembly, typeof(ApplicationLayer).Assembly);
+
+    //    builder.Services.AddDataProtection().PersistKeysToFileSystem(new DirectoryInfo("/app/keys"));
+
+        builder.Services.AddMediatR(cfg =>
         {
-            Log.Information("Starting web application");
-
-            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-            builder.AddDefaultLogging();
-
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-
-            builder.AddBasicHealthChecks();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddDbContext<DefaultContext>(options =>
-                options.UseNpgsql(
-                    builder.Configuration.GetConnectionString("DefaultConnection"),
-                    b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.ORM")
-                )
+            cfg.RegisterServicesFromAssemblies(
+                typeof(ApplicationLayer).Assembly,
+                typeof(Program).Assembly
             );
+        });
 
-            builder.Services.AddJwtAuthentication(builder.Configuration);
+        builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
-            builder.RegisterDependencies();
+        var app = builder.Build();
 
-            builder.Services.AddAutoMapper(typeof(Program).Assembly, typeof(ApplicationLayer).Assembly);
-
-            builder.Services.AddMediatR(cfg =>
-            {
-                cfg.RegisterServicesFromAssemblies(
-                    typeof(ApplicationLayer).Assembly,
-                    typeof(Program).Assembly
-                );
-            });
-
-            builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-
-            var app = builder.Build();
-            app.UseMiddleware<ValidationExceptionMiddleware>();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseBasicHealthChecks();
-
-            app.MapControllers();
-
-            app.Run();
-        }
-        catch (Exception ex)
+        using (var scope = app.Services.CreateScope())
         {
-            Log.Fatal(ex, "Application terminated unexpectedly");
+            var db = scope.ServiceProvider.GetRequiredService<DefaultContext>();
+            db.Database.Migrate();
         }
-        finally
+  
+
+        app.UseMiddleware<ValidationExceptionMiddleware>();
+
+        if (app.Environment.IsDevelopment())
         {
-            Log.CloseAndFlush();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseBasicHealthChecks();
+
+        app.MapControllers();
+
+        app.Run();
     }
+    catch (Exception ex)
+    {
+        Log.Fatal(ex, "Application terminated unexpectedly");
+    }
+    finally
+    {
+        Log.CloseAndFlush();
+    }
+}
 }
